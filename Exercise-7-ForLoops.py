3d07b26f84df4339b2b3b44f51bad595ff54bab3
Teerapoom <88318468+Teerapoom@users.noreply.github.com>
2021-11-24 21:32:29 +0700

    Add files via upload

---
+++ Exercise-7-ForLoops.py
@@ -0,0 +1,51 @@
+
+# Nameber = int(input("จํานวนเต็มบวก: "))
+# for i in range(0,Nameber):
+#     print(i+1,end=",")
+
+#12
+# Nameber = int(input("จํานวนเต็มบวก: "))
+# count = 0
+# for i in range(0,Nameber):
+#     add = i+1
+#     print(add)
+#     mob = Nameber % add
+#     # print(add,"ตัวที่หารลงตัว",mob)
+#     if mob == 0:
+#         count += 1
+# print("หารลงตัวมีทั้งหมด",count)
+# #อีกวิธี
+# Nameber = int(input("จํานวนเต็มบวก: "))
+# count = 0
+# for i in range(0,Nameber+1):
+#     if Nameber % i == 0:
+#         count += 1
+#         print(i)
+# print("หารลงตัว = %d" %count )
+
+# 13
+# Nameber = int(input("จํานวนเต็มบวก: "))
+# for i in range(0,Nameber):
+#     print(i+1,end=",")
+#     if i % 2 != 0:
+#         print("|"+"จำนวนเฉพาะ",i)
+
+#14
+# Nameber = int(input("จํานวนเต็มบวก: "))
+# sum = 1
+# for i in range(Nameber,0,-1):
+#     sum *= i 
+# print(sum)
+
+#15
+# Nameber = int(input("จํานวนเต็มบวก: "))
+# for i in range(1,13):
+#     print(Nameber,"x", i ," = ", Nameber*i )
+
+
+# i = 1
+# Nameber = int(input())
+# while i <= 12:
+#     add = Nameber*i
+#     print(Nameber,"x", i,"=",add)
+#     i += 1
