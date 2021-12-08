148fd169902a74744fdaf437560076e48ca03332
Teerapoom <88318468+Teerapoom@users.noreply.github.com>
2021-12-09 00:27:32 +0700

    Add files via upload

---
+++ Homework/homework_EP.2.py
@@ -0,0 +1,22 @@
+# Number_1 = float(input("กรอกตัวเลขที่ 1: "))
+# Number_2 = float(input("กรอกตัวเลขที่ 2: "))
+# print("ท่านต้องการให้นำตัวเลขมาทำอะไร")
+# print(""" \t กด 1 บวก หรือ (+)
+#             กด 2 ลบ หรือ  (-)
+#             กด 3 คูณ หรือ (*)
+#             กด 4 หาร หรือ (/) """)
+# symbol = input("ท่านต้องการที่จะ ")
+# if symbol == "1" or symbol == "+":
+#     sum = Number_1 + Number_2
+#     print(Number_1 ,"+",Number_2,"=","%.2f" %sum)
+# elif symbol == "2" or symbol == "-":
+#     delete = Number_1 - Number_2
+#     print(Number_1 ,"-",Number_2,"=","%.2f" %delete)
+# elif symbol == "3" or symbol == "*":
+#     multiply = Number_1 * Number_2
+#     print(Number_1 ,"x",Number_2,"=","%.2f" %multiply)
+# elif symbol == "4" or symbol == "/":
+#     divide = Number_1 / Number_2
+#     print(Number_1 ,"÷",Number_2,"=","%.2f" %divide)
+# else:
+#     print("โปรเเกรมนี้ยังไม่รองรับฟังชั่นอื่นนอกจาก(บวก,ลบ,คูณ,หาร)")
