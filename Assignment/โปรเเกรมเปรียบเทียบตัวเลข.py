0c51bfcdb501629181e0f1a8233b8c04cb1bddf8
Teerapoom <88318468+Teerapoom@users.noreply.github.com>
2021-12-10 22:41:35 +0700

    Add files via upload

---
+++ Assignment/โปรเเกรมเปรียบเทียบตัวเลข.py
@@ -0,0 +1,9 @@
+Namber_1 = float(input("ป้อนตัวเลขตัวที่ 1 :"))
+Namber_2 = float(input("ป้อนตัวเลขตัวที่ 2 :"))
+
+if Namber_1 > Namber_2:
+    print(Namber_1,"มากกว่า",Namber_2)
+elif Namber_1 < Namber_2:
+    print(Namber_1 ,"น้อยกว่า" ,Namber_2)
+else:
+    print(Namber_1,"เท่ากัน",Namber_2)
